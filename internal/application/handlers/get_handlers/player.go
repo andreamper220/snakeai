1cf99d3fefbee97d6f547e07a02ccc159d49594b
AVVolkov <avvolkov@bellintegrator.com>
2024-06-25 11:18:37 +0300

    remove get handlers tests

--- internal/application/handlers/get_handlers/player.go
+++ internal/application/handlers/get_handlers/player.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"snake_ai/pkg/logger"
 )
 
 func PlayerMatch(w http.ResponseWriter, r *http.Request, userId uuid.UUID) {
@@ -14,7 +13,6 @@ func PlayerMatch(w http.ResponseWriter, r *http.Request, userId uuid.UUID) {
 	filePath := filepath.Join(dir, "internal/templates/play.html")
 	tmpl, err := template.ParseFiles(filePath)
 	if err != nil {
-		logger.Log.Error(err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
