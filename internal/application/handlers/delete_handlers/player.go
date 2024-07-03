c3aa902c23be2d9d914dd4d2af9b02356d5a97da
AVVolkov <avvolkov@bellintegrator.com>
2024-07-03 18:52:11 +0300

    fixes

--- internal/application/handlers/delete_handlers/player.go
+++ internal/application/handlers/delete_handlers/player.go
@@ -10,13 +10,11 @@ import (
 func PlayerRemoveAi(w http.ResponseWriter, r *http.Request, userId uuid.UUID) {
 out:
 	for _, g := range gamedata.CurrentGames.Games {
-		g.RLock()
 		for _, p := range g.Party.Players {
 			if p.Id == userId {
 				g.RemoveSnake(userId)
 				break out
 			}
 		}
-		g.RUnlock()
 	}
 }
