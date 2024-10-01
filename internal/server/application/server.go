7a3e1ab1248ff82949a9b192a00fb0a9bdf48bd2
AVVolkov <avvolkov@bellintegrator.com>
2024-10-01 13:04:00 +0300

    ssl issues [WIP]

--- internal/server/application/server.go
+++ internal/server/application/server.go
@@ -136,18 +136,17 @@ func Run(serverless bool) error {
 		return nil
 	}
 
-	//certManager := autocert.Manager{
-	//	Prompt:     autocert.AcceptTOS,
-	//	Cache:      autocert.DirCache("/etc/ssl/certs"),
-	//	HostPolicy: autocert.HostWhitelist("snakeai.netvolk.online"),
-	//}
-	//
-	//server := &http.Server{
-	//	Addr:      ":443",
-	//	Handler:   MakeRouter(),
-	//	TLSConfig: certManager.TLSConfig(),
-	//}
-	//
-	//return server.ListenAndServeTLS("", "")
-	return http.Serve(autocert.NewListener("snakeai.netvolk.online"), MakeRouter())
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		Cache:      autocert.DirCache("/etc/ssl/certs"),
+		HostPolicy: autocert.HostWhitelist("snakeai.netvolk.online", "cloudflare-ech.com"),
+	}
+
+	server := &http.Server{
+		Addr:      ":443",
+		Handler:   MakeRouter(),
+		TLSConfig: certManager.TLSConfig(),
+	}
+
+	return server.ListenAndServeTLS("", "")
 }
